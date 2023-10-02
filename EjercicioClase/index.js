8b43095fac90a69b888abad0df4e6090f03ea042
ariolop154 <ariolop154@g.educaand.es>
2023-10-02 12:23:13 +0200

    Cambio de nombre y use strict

--- EjercicioClase/index.js
+++
@@ -1,2 +0,0 @@
-let nombre = prompt("¿Cuál es tu nombre?");
-alert(`Tu nombre es ${nombre}`)
---
+++ EjercicioClase/interaccion.js
@@ -0,0 +1,4 @@
+'use strict';
+
+let nombre = prompt("¿Cuál es tu nombre?");
+alert(`Tu nombre es ${nombre}`);
