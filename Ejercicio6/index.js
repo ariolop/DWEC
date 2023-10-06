c893dc0ce99c6353779bd1f85058070e7dab9aba
ariolop154 <ariolop154@g.educaand.es>
2023-10-06 10:42:38 +0200

    Ventana Modal

--- Ejercicio6/index.js
+++ Ejercicio6/index.js
@@ -1,14 +1,56 @@
-function cambiarIdioma(idioma) {
+'use strict'
 
-    switch (idioma) {
-        case "espanol":
-            alert("Buenos dias");
-            break;
-        case "ingles":
-            alert("Good morning");
-            break;
-        case "ruso":
-            alert("доброе утро");
-            break;
+var modal = document.getElementById("myModal");
+var btnEs = document.getElementById("buttonEs");
+var btnIn = document.getElementById("buttonIn");
+var btnRu = document.getElementById("buttonRu");
+var span = document.getElementsByClassName("close")[0];
+var body = document.getElementsByTagName("body")[0];
+
+btnEs.onclick = function() {
+    modal.style.display = "block";
+
+    body.style.position = "static";
+    body.style.height = "100%";
+    body.style.overflow = "hidden";
+
+    document.getElementById("parrafo").innerHTML = "Buenos dias";
+}
+
+btnIn.onclick = function() {
+    modal.style.display = "block";
+
+    body.style.position = "static";
+    body.style.height = "100%";
+    body.style.overflow = "hidden";
+
+    document.getElementById("parrafo").innerHTML = "Good morning";
+}
+
+btnRu.onclick = function() {
+    modal.style.display = "block";
+
+    body.style.position = "static";
+    body.style.height = "100%";
+    body.style.overflow = "hidden";
+
+    document.getElementById("parrafo").innerHTML = "доброе утро";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+
+    body.style.position = "inherit";
+    body.style.height = "auto";
+    body.style.overflow = "visible";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
     }
 }
