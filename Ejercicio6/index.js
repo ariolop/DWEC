41c76e880986c1c222b5c538517ab9c5f8dc8d55
ariolop154 <ariolop154@g.educaand.es>
2023-10-06 08:53:17 +0200

    Entrega ejercicios

---
+++ Ejercicio6/index.js
@@ -0,0 +1,14 @@
+function cambiarIdioma(idioma) {
+
+    switch (idioma) {
+        case "espanol":
+            alert("Buenos dias");
+            break;
+        case "ingles":
+            alert("Good morning");
+            break;
+        case "ruso":
+            alert("доброе утро");
+            break;
+    }
+}
---
+++ Ejercicio7/index.js
@@ -0,0 +1,14 @@
+function cambiarIdioma(idioma) {
+
+    switch (idioma) {
+        case "espanol":
+            console.log("Buenos dias");
+            break;
+        case "ingles":
+            console.log("Good morning");
+            break;
+        case "ruso":
+            console.log("доброе утро");
+            break;
+    }
+}
