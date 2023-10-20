591b67675111936a8ae1020af5fa21ab24a1ddec
ariolop154 <ariolop154@g.educaand.es>
2023-10-20 12:40:58 +0200

    Dibujando con bucles

--- EjercicioClase/bucles.js
+++ EjercicioClase/bucles.js
@@ -85,4 +85,212 @@ function pintarTrianguloMedio(tamaño, caracter = ASTERICO)
     console.log(figura);
 }
 
-pintarTrianguloDerecha(5);
+function pintarRomboNumerico(tamaño)
+{
+    let figura = "";
+
+    for(let i = 0; i < tamaño; i++)
+    {
+        let fila = "";
+        let espacios = "";
+        let caracteres = "";
+
+        for(let j = tamaño-1; j > i; j--)
+        {
+            espacios += " ";
+        }
+        
+        for(let k = 1; k <= (i*2)+1; k++)
+        {
+            caracteres += k
+        }
+
+        fila = espacios + caracteres + espacios;
+
+        figura += fila + "\n";
+    }
+
+    for(let i = tamaño-1; i > 0; i--)
+    {
+        let fila = "";
+        let espacios = "";
+        let caracteres = "";
+
+        for(let j = tamaño; j > i; j--)
+        {
+            espacios += " ";
+        }
+        
+        for(let k = 1; k < (i*2); k++)
+        {
+            caracteres += k
+        }
+
+        fila = espacios + caracteres + espacios;
+
+        figura += fila + "\n";
+    }
+
+    console.log(figura);
+}
+
+function pintarRomboNumerico2(tamaño)
+{
+    let figura = "";
+
+    for(let i = 0; i < tamaño; i++)
+    {
+        let fila = "";
+        let espacios = "";
+        let caracteres = "";
+
+        for(let j = tamaño-1; j > i; j--)
+        {
+            espacios += " ";
+        }
+        
+        
+        for(let k = 1, mayorqueK = 0; k <= (i*2)+1; k++)
+        {
+            if(k > i+1)
+            {
+                mayorqueK += 2;
+                caracteres += (k - mayorqueK);
+            }
+            else
+            {
+                caracteres += k;
+            }
+        }
+
+        fila = espacios + caracteres + espacios;
+
+        figura += fila + "\n";
+    }
+
+    for(let i = tamaño-1; i > 0; i--)
+    {
+        let fila = "";
+        let espacios = "";
+        let caracteres = "";
+
+        for(let j = tamaño; j > i; j--)
+        {
+            espacios += " ";
+        }
+        
+        for(let k = 1, mayorqueK = 0; k < (i*2); k++)
+        {
+            if(k >= i+1)
+            {
+                mayorqueK += 2;
+                caracteres += (k - mayorqueK);
+            }
+            else
+            {
+                caracteres += k;
+            }
+        }
+
+        fila = espacios + caracteres + espacios;
+
+        figura += fila + "\n";
+    }
+
+    console.log(figura);
+}
+
+function pintarRomboHueco(tamaño){
+
+    let figura = "";
+
+    for(let i = 0; i < tamaño; i++)
+    {
+        let fila = "";
+        let espaciosExternos = "";
+        let espaciosInternos = "";
+
+        for(let j = tamaño-1; j > i; j--)
+        {
+            espaciosExternos += " ";
+        }
+
+        for(let k = 0; k < (i*2)-1; k++)
+        {
+            espaciosInternos += " ";
+        }
+
+        fila += espaciosExternos + "*";
+
+        if(espaciosInternos)
+        {
+            fila += espaciosInternos + "*";
+        }
+         
+        figura += fila + "\n";
+    }
+
+    for(let i = tamaño-2; i >= 0; i--)
+    {
+        let fila = "";
+        let espaciosExternos = "";
+        let espaciosInternos = "";
+
+        for(let j = tamaño-1; j > i; j--)
+        {
+            espaciosExternos += " ";
+        }
+
+        for(let k = (i*2)-1; k > 0; k--)
+        {
+            espaciosInternos += " ";
+        }
+
+        fila += espaciosExternos + "*";
+
+        if(espaciosInternos)
+        {
+            fila += espaciosInternos + "*";
+        }
+         
+        figura += fila + "\n";
+    }
+
+    console.log(figura);
+}
+
+function pintaTablero(tamaño)
+{
+    let figura = "";
+    let elecCaracterColumna = true;
+
+    for(let bloque = 1; bloque <= tamaño; bloque++)
+    {
+        for(let i = 1; i <= tamaño; i++) //Filas
+        {
+            let fila = "";
+
+            let elecCaracterFila = elecCaracterColumna;
+            for(let j = 1; j <= tamaño; j++) //Bloque columnas
+            {
+                let caracter = elecCaracterFila ? "#" : "-";
+
+                for(let k = 0; k < tamaño; k++) //Columnas y diferenciacion de caracteres
+                {
+                    fila += caracter;   
+                }
+
+                elecCaracterFila = !elecCaracterFila;
+            }
+        
+            figura += fila + "\n";
+        }
+
+        elecCaracterColumna = !elecCaracterColumna;
+
+    }    
+
+    console.log(figura);
+}
+
+pintaTablero(6);
