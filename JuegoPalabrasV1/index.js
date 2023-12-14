cff6836fa1edcd52a7fb5a3b5e86d933fe3c244e
ariolop154 <ariolop154@g.educaand.es>
2023-12-14 10:18:50 +0100

    Seguir por la puntuacion adicional

--- JuegoPalabrasV1/index.js
+++ JuegoPalabrasV1/index.js
@@ -1,3 +1,5 @@
+"use strict"
+
 /* Objecto "tiempo" */
 let tiempo = {
 
--- JuegoPalabrasV2/index.js
+++ JuegoPalabrasV2/index.js
@@ -1,3 +1,22 @@
+/* Importamos el diccionario */
+import {diccionario} from "https://cdn.jsdelivr.net/gh/fran-dawbaza/spanish-dictionary/diccionario.js";
+
+/* Constantes de ERROR */
+const ERROR_ININIO_LETRA = 101;
+
+/* Elimina los diacríticos de un texto excepto si es una "ñ" (ES6) */
+function eliminarDiacriticosEs(texto) {
+    return texto
+           .normalize('NFD')
+           .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1")
+           .normalize();
+}
+
+const diccionarioSinTildes = diccionario.map(eliminarDiacriticosEs);
+
+const diccionarioSet = new Set(diccionarioSinTildes);
+
+
 /* Objecto "tiempo" */
 let tiempo = {
 
@@ -9,14 +28,20 @@ let tiempo = {
         console.log("Cronometro parado");
 
         document.getElementById("introducirPalabras").setAttribute("disabled",true);
+        document.getElementById("introducirPalabras").value = "";
         document.getElementById("enviarPalabra").setAttribute("disabled",true);
         document.getElementById("empezarPartida").setAttribute("disabled",true);
         document.getElementById("nuevaPartida").removeAttribute("disabled");
 
-        document.getElementById("cantPalabras").innerHTML = palabras.length;
-        document.getElementById("listaPalabras").innerHTML = palabras.toString();
+        document.getElementById("cantPalabras").innerHTML = listaPalabras.length;
+        document.getElementById("listaPalabras").innerHTML = listaPalabras.toString();
 
-        document.getElementsByClassName("resultado").item(0).style.display = "block";
+        document.getElementsByClassName("resultado").item(0).style.display = "flex";
+        
+        document.getElementById("alertaCorrecta").style.display = "none";
+        document.getElementById("alertaIncorrecta").style.display = "none";
+
+        document.getElementById("totalPuntos").innerHTML = puntos;
     },
 
     reiniciarCronometro: function()
@@ -54,18 +79,45 @@ function generarLetra()
     document.getElementById("empezarPartida").disabled = false;
     document.getElementById("nuevaPartida").removeAttribute("disabled");
     document.getElementById("tiempo").innerHTML = 10;
+    document.getElementsByClassName("resultado").item(0).style.display = "none";
 
     const posAlea = Math.floor((Math.random())*(letras.length));
     const letraAleatoria = letras[posAlea];
 
     document.getElementById("letra").innerHTML = letraAleatoria;
 
+    puntosPorPalabra = puntosPorLetraInicial();
+
     return letraAleatoria;
 }
 
-/* Array con las palabras introducidas */
-const palabras = [];
+/* Dependiendo de la letra, la palabra introducida tendrá unos puntos u otros */
+function puntosPorLetraInicial(letra) {
+    switch (letra) {
+        case 'A', 'C', 'D', 'E':
+            return 1;
+        case 'M', 'P', 'R', 'S', 'T':
+            return 2;
+        case 'B', 'F', 'G', 'H', 'I', 'V':
+            return 3;
+        case 'J', 'L', 'N', 'O', 'Z':
+            return 4;
+        case 'K', 'Ñ', 'Q', 'U', 'W', 'X', 'Y':
+            return 5;
+    }
+}
 
+/* Array con las palabras introducidas */
+const listaPalabras = [];
+let puntos = 0;
+let puntosPorPalabra = 0;
+const puntosPorLongitudPalabra = new Map([
+    [8,1],      [6,2],      [5,3],      [4,4],      [1,5],
+    [9,1],      [7,2],      [15,3],     [16,4],     [2,5],
+    [10,1],     [13,2],                 [17,4],     [3,5],
+    [11,1],     [14,2],                             [18,5],
+    [12,1]
+]);
 
 /* Empezar partida */
 function empezarPartida(event) {
@@ -73,23 +125,28 @@ function empezarPartida(event) {
     document.getElementById("enviarPalabra").disabled = false;
     document.getElementById("nuevaPartida").disabled = true;
     document.getElementById("empezarPartida").disabled = true;
-    palabras.splice(0,palabras.length);
+    listaPalabras.splice(0,listaPalabras.length);
 
     tiempo.iniciarCronometro(9, 0);
     document.getElementById("introducirPalabras").focus();
     document.getElementsByClassName("resultado").item(0).style.display = "none";
 }
 
-/* Reiniciar cronometro si introduce una palabra correcta */
-function reiniciarCronometro(event)
+/* Si introduce una palabra correcta, reiniciamos tiempo y mostramos mensaje "correcto"; sino solo mostramos mensaje "incorrecto" */
+function palabraIntroducida(event)
 {
     event.preventDefault();
-    const palabra = String(document.getElementById("introducirPalabras").value);
 
-    if(palabra.startsWith(letraAleatoria))
+    document.getElementById("alertaCorrecta").style.display = "none";
+    document.getElementById("alertaIncorrecta").style.display = "none";
+
+    const palabra = String(document.getElementById("introducirPalabras").value).toLowerCase();
+
+    if(validarPalabra(palabra))
     {
         tiempo.reiniciarCronometro();
-        palabras.push(palabra);
+        listaPalabras.push(palabra);
+        calcularPuntos(palabra);
     }
 
 
@@ -97,10 +154,68 @@ function reiniciarCronometro(event)
     document.getElementById("introducirPalabras").focus();
 }
 
+/* ¿Es válida la palabra? */
+function validarPalabra(palabra)
+{
+    let palabraCorrecta;
+
+    if( ! palabra.startsWith(letraAleatoria))
+    {
+        //Mostrar mensaje: "La palabra no empieza por la letra indicada"
+        palabraCorrecta = false;
+
+        document.getElementById("alertaIncorrecta").innerHTML = "La palabra no empieza por la letra indicada";
+        document.getElementById("alertaIncorrecta").style.display = "block";
+    }
+    else if( listaPalabras.includes(palabra) )
+    {
+        //Mostrar mensaje: "La palabra ya ha sido introducida"
+        palabraCorrecta = false;
+        
+        document.getElementById("alertaIncorrecta").innerHTML = "La palabra ya ha sido introducida";
+        document.getElementById("alertaIncorrecta").style.display = "block";
+    }
+    else if( ! diccionarioSet.has(palabra) )
+    {
+        //Mostrar mensaje: "La palabra no existe en el diccionario"
+        palabraCorrecta = false;    
+
+        document.getElementById("alertaIncorrecta").innerHTML = "La palabra no existe en el diccionario";
+        document.getElementById("alertaIncorrecta").style.display = "block";
+    }
+    else
+    {
+        //Palabra correcta
+        palabraCorrecta = true;
+        document.getElementById("alertaCorrecta").style.display = "block";
+    }
+
+    return palabraCorrecta;
+}
+
+/* Calcular puntos de la palabra */
+function calcularPuntos(palabra) {
+    puntos += puntosPorPalabra;
+
+    puntos += (palabra.length < 18) ?  puntosPorLongitudPalabra.get(palabra.length) : 5;
+
+    puntos += puntosAdicionales(palabra);
+}
+
+/* Calcular puntos adicionales */
+function puntosAdicionales(palabra)
+{
+    const puntosAdicionales = 0;
+
+    
+
+    return puntosAdicionales;
+}
+
 
 /* Agregar eventos */
 document.getElementById("empezarPartida").addEventListener("click", empezarPartida);
-document.getElementById("form").addEventListener("submit", reiniciarCronometro);
+document.getElementById("form").addEventListener("submit", palabraIntroducida);
 document.getElementById("nuevaPartida").addEventListener("click", () => {letraAleatoria = generarLetra()});
 
 /* Funciones que se ejecutan cuando carga la página */
