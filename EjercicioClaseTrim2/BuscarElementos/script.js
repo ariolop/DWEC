78f7070e8286fce9c945802a9f90e255679bf709
ariolop154 <ariolop154@g.educaand.es>
2024-01-12 10:18:41 +0100

    Ejercicio 12/01

---
+++ EjercicioClaseTrim2/BuscarElementos/script.js
@@ -0,0 +1,8 @@
+"use scrict";
+
+console.log(document.getElementById("age-table"));
+console.log(document.getElementById("age-table").getElementsByTagName("label"));
+console.log(document.querySelector("td"));
+console.log(document.getElementsByName("search")[0]);
+console.log(document.getElementsByName("search")[0].querySelector("input"));
+console.log(document.getElementsByName("search")[0].lastElementChild);
