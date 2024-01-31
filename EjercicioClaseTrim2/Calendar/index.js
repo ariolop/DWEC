c1f3a258f6b02e6feb1f6fd53ce7aa3df3e20c05
Alejandro <ariolop154@gmail.com>
2024-01-31 18:06:55 +0100

    Calendario terminado

--- EjercicioClaseTrim2/Calendar/index.js
+++ EjercicioClaseTrim2/Calendar/index.js
@@ -7,49 +7,44 @@ function createCalendar(elem, year, month) {
     const tabla = document.createElement("table");
 
     /* Genero fila con cabecera de la semana */
-    const cabecera = document.createElement("th");
-    const diasDeLaSemana = ["SU","MO","TU","WE","TH","FR","SA"];
+    const cabecera = document.createElement("tr");
+    const diasDeLaSemana = ["MO", "TU", "WE","TH", "FR","SA","SU"];
 
-    for (let i = 1; i < diasDeLaSemana.length; i++) {
-        const celda = document.createElement("td");
+    for (let i = 0; i < diasDeLaSemana.length; i++) {
+        const celda = document.createElement("th");
         celda.textContent = diasDeLaSemana[i];
         cabecera.append(celda);
     }
 
-        /* Domingo */
-    const celda = document.createElement("td");
-    celda.textContent = diasDeLaSemana[0];
-    cabecera.append(celda);
-
     tabla.append(cabecera);
     
     /* Colocar los días en el día de la semana */
-    debugger;
+    // debugger;
     while(fecha.getMonth()+1 === month)
     {
         console.log(fecha.getDate() + " " + (fecha.getDay()) + " " + diasDeLaSemana[fecha.getDay()]);
 
         const fila = document.createElement("tr");
 
-        for (let i = 1; i != 0; i++) {
-            
+        for(let i = 0; i < 7; i++)
+        {
+            const diaDeLaSemana = fecha.getDay() !== 0 ? fecha.getDay() - 1 : 6;
+
+            console.log(diaDeLaSemana);
+
             const celda = document.createElement("td");
 
-            if(i === fecha.getDate())
+            if(i === diaDeLaSemana && fecha.getMonth()+1 === month)
             {
                 celda.textContent = fecha.getDate();
                 fecha.setDate(fecha.getDate()+1);
             }
             else
             {
-                celda.textContent = "";
+                celda.textContent = " ";
             }
 
             fila.append(celda);
-
-            i = i === 6 ? i = -1 : i;
-
-            console.log(i);
         }
 
         tabla.append(fila);
@@ -60,4 +55,4 @@ function createCalendar(elem, year, month) {
 }
 
 const cal = document.getElementById("calendar");
-createCalendar(cal, 2012, 9);
+createCalendar(cal, 2024, 1);
