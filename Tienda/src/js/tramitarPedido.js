a82711629a1e0b4ecf474c5702a85be62dbd5c6e
Alejandro <ariolop154@gmail.com>
2024-03-08 20:07:08 +0100

    Tramitar Pedido PUSH and PUT

--- Tienda/src/js/tramitarPedido.js
+++ Tienda/src/js/tramitarPedido.js
@@ -4,6 +4,9 @@ Categorias.cargarCategoriasMenu();
 
 cargarInformacionPrecios();
 
+añadirEventoValorVacio();
+añadirEventoGuionNumeroTarjeta();
+
 function cargarInformacionPrecios() {
     const informacionPago = JSON.parse(localStorage.getItem("pedido"));
 
@@ -47,3 +50,100 @@ document.getElementById("metodosDePago").addEventListener("change", e => {
         document.getElementById("titularTarjeta").setAttribute("disabled","");
     }
 });
+
+function añadirEventoValorVacio() {
+    const input = document.getElementsByClassName("verificado");
+    console.log(input);
+
+    Array.from(input).forEach(element => {
+        element.addEventListener("change", e => {
+            if(e.target.value.trim().length === 0)e.target.value = "";
+        });
+    });
+}
+
+function añadirEventoGuionNumeroTarjeta() {
+    document.getElementById("numeroTarjeta").addEventListener("input", e => {
+        
+        const temp = e.target.value.substring(e.target.value.length - 4, e.target.value.length);
+        console.log(temp);
+
+        if(temp.length === 4 && +temp > 0 && e.target.value.length < 16)
+        {
+            e.target.value += "-";
+        }
+    });
+}
+
+document.getElementById("formularioDatos").addEventListener("submit", async e => {
+    e.preventDefault();
+
+    const informacion = await obtenerInformacion();
+
+    const usuario = localStorage.getItem("sesionLocal");
+    const carrito = JSON.parse(localStorage.getItem(usuario))
+
+    actualizarProductosVendidos(carrito);
+    //realizarPedido(informacion);
+});
+
+async function obtenerInformacion() {
+    const usuario = localStorage.getItem("sesionLocal");
+    const ultimoID = await obtenerUltimoID();
+
+    return {
+        "id": ultimoID + 1,
+        "idUsuario": null,
+        "carrito": JSON.parse(localStorage.getItem(usuario)),
+        "direccionEnvio": {
+            "direccionCalle" : document.getElementById("direccion").value + " " + document.getElementById("datosAdicionales").value,
+            "direccionCodigoPostal": document.getElementById("codigoPostal").value,
+            "direccionProvincia": document.getElementById("provincia").value,
+            "direccionPais": document.getElementById("pais").value
+        },
+        "metodoPago": document.querySelector("input[name='metodoPago']:checked").value
+    };
+}
+
+function realizarPedido(informacion) {
+    fetch("http://localhost:3000/pedidos", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(informacion)
+    })
+    .then(data => data.json())
+    .then(resul => console.log(resul));
+}
+
+function obtenerUltimoID() {
+    return fetch("http://localhost:3000/pedidos")
+    .then(resul => resul.json())
+    .then(pedidos => {
+        return +pedidos[pedidos.length - 1].id;
+    })
+}
+
+function actualizarProductosVendidos(carrito) {
+    
+    Object.keys(carrito).forEach(producto => {
+
+        fetch(`http://localhost:3000/funkos/${producto}`)
+        .then(resul => resul.json())
+        .then(datosProducto => {
+            datosProducto.cantidadVendidos += 1;
+            return JSON.stringify(datosProducto);
+        })
+        .then(datosProducto => {
+            fetch(`http://localhost:3000/funkos/${producto}`,{
+                method: "PUT",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: datosProducto
+            })
+        });
+    })
+    
+}
